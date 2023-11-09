69d392a6ab051290b5eba95d79212d5050ea3c9e
MarcheloJacome <marcelo.jacome@hotmail.com>
2023-11-09 17:18:27 -0500

    Sidebar
    Sidebar added

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import {Sidebar, Videos} from './';
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{ flexDirection: { sx:
+    "column", md: "row"}}}>
+      <Box sx={{ height: { sx: 'auto',
+       md: '92hv'}, borderRight:'1px solid #3d3d3d',
+       px: { sx: 0, md: 2}}}>
+        <Sidebar/>
+
+        <Typography className="copyright"
+        variant='body2' sx={{ mt:1.5, color: '#fff' }}>
+          Copyright 2023 MJ
+        </Typography>
+      </Box>
+      <Box p={2} sx={{ 
+        overflow:'auto', height:'90vh',flex:2 }}
+      >
+        <Typography variant='h4' fontWeight="bold" mb={2} sx={{
+          color: 'white'}}
+        >
+          <span style={{ color: '#7A7A7A'}}> 
+            Videos
+          </span>
+        </Typography>
+        <Videos videos={[]}/>
+      </Box>
+    </Stack>
   )
 }
 
