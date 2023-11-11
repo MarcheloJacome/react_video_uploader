e120823f654dea27609a0e89e578c80a97e5d0e1
MarcheloJacome <marcelo.jacome@hotmail.com>
2023-11-10 20:06:12 -0500

    Videos api
    Implementation of vieos API

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,15 +1,31 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 import {Sidebar, Videos} from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) => 
+      setVideos(data.items)
+    )
+
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx:
     "column", md: "row"}}}>
       <Box sx={{ height: { sx: 'auto',
        md: '92hv'}, borderRight:'1px solid #3d3d3d',
        px: { sx: 0, md: 2}}}>
-        <Sidebar/>
+        <Sidebar
+          selectedCategory = {selectedCategory}
+          setSelectedCategory = {setSelectedCategory}
+        />
 
         <Typography className="copyright"
         variant='body2' sx={{ mt:1.5, color: '#fff' }}>
@@ -22,7 +38,7 @@ const Feed = () => {
         <Typography variant='h4' fontWeight="bold" mb={2} sx={{
           color: 'white'}}
         >
-          <span style={{ color: '#7A7A7A'}}> 
+          {selectedCategory}<span style={{ color: '#7A7A7A'}}> 
             Videos
           </span>
         </Typography>
