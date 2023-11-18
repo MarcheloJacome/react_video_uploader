ebba5aab41bdfca6ba9b427d053d49b1c3fbe3e9
MarcheloJacome <marcelo.jacome@hotmail.com>
2023-11-17 20:08:16 -0500

    Final components
    Final components added

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,20 +3,39 @@ import { Stack, Box } from "@mui/material";
 
 import { ChannelCard, VideoCard } from "./";
 
-const Videos = ({ videos }) => {
-  return (
+const Videos = ({ videos, direction }) => {
+  if(!videos.length) return 'Loading';
+
+  if(videos.length===1){
+    return (
 
 
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
-      {videos[0].map((item, idx) => (
-        <Box key={idx}>
-          {item.id.videoId && <VideoCard video={item} /> }
-          {item.id.channelId && <ChannelCard channelDetail={item} />}
-        </Box>
-      ))}
-    </Stack>
-    
-  );
+      <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+        {videos[0].map((item, idx) => (
+          <Box key={idx}>
+            {item.id.videoId && <VideoCard video={item} /> }
+            {item.id.channelId && <ChannelCard channelDetail={item} />}
+          </Box>
+        ))}
+      </Stack>
+      
+    );
+  }else{
+    return (
+
+
+      <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+        {videos.map((item, idx) => (
+          <Box key={idx}>
+            {item.id.videoId && <VideoCard video={item} /> }
+            {item.id.channelId && <ChannelCard channelDetail={item} />}
+          </Box>
+        ))}
+      </Stack>
+      
+    );
+  }
+
 }
 
 export default Videos;
